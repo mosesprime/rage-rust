76a67df439b7f3f496ccff0072ec34d746cb091c
mosesprime <huntermoses007@gmail.com>
2023-11-11 00:22:04 -0500

    considering btreemap and ast layout

--- compiler/ragec-parser/src/lib.rs
+++ compiler/ragec-parser/src/lib.rs
@@ -1,26 +1,32 @@
-use ragec_token::Token;
-use std::collections::HashMap;
+use ragec_ast::AstNodeId;
+use ragec_token::{Token, TokenKind, WhitespaceKind};
+use std::collections::BTreeMap;
 
 pub struct Parser {
-    next_id: usize,
-    table: HashMap<usize, ()>
+    symbol_table: BTreeMap<AstNodeId, Token>,
+    _next_id: usize,
 }
 
 impl Parser {
     pub fn new() -> Self {
         Self { 
-            next_id: 0,
-            table: Default::default(),
+            symbol_table: Default::default(),
+            _next_id: 0,
         }
     }
 
-    pub fn run(&self) -> Result<(), ParsingError> {
+    pub fn run(&mut self, mut input: Vec<Token>) -> Result<(), ParsingError> {
+        input.retain(|t| t.kind != TokenKind::Whitespace(WhitespaceKind::Blank)); // remove blank whitespace, retain newline
+        for token in input {
+            let id = self.next_id();
+            let _ = self.symbol_table.insert(id, token);
+        }
         Ok(())
     }
 
     fn next_id(&mut self) -> usize {
-        let n = self.next_id;
-        self.next_id += 1;
+        let n = self._next_id;
+        self._next_id += 1;
         n
     }
 }
